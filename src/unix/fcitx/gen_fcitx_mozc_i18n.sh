0d328aa253aa36a7acec8b5167a6b71b1d27c073
Hiroki Sato <hrs@allbsd.org>
2017-01-01 13:14:03 +0900

    Import fcitx-mozc-2.18.2612.102.1.patch.
    Obtained from:	https://download.fcitx-im.org/fcitx-mozc/.

--- src/unix/fcitx/gen_fcitx_mozc_i18n.sh
+++ src/unix/fcitx/gen_fcitx_mozc_i18n.sh
@@ -8,13 +8,3 @@ for pofile in po/*.po
 do
     msgfmt "$pofile" -o "$1/`basename ${pofile} .po`.mo"
 done
-#!/bin/sh
-
-objdir="$1"
-
-mkdir -p "$1"
-
-for pofile in po/*.po
-do
-    msgfmt "$pofile" -o "$1/`basename ${pofile} .po`.mo"
-done
