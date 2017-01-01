9560c25d0d79796d971a7e83b2a9b673a91db2af
Hiroki Sato <hrs@allbsd.org>
2017-01-01 14:33:07 +0900

    FreeBSD has pthread_set_name_np() instead of pthread_setname_np().

--- src/base/thread.cc
+++ src/base/thread.cc
@@ -147,9 +147,11 @@ void Thread::Start(const string &thread_name) {
     // NaCl doesn't support setname.
 #elif defined(OS_MACOSX)
     pthread_setname_np(thread_name.c_str());
-#else  // !(OS_NACL | OS_MACOSX)
+#elif defined(OS_FREEBSD)
+    pthread_set_name_np(*state_->handle_, thread_name.c_str());
+#else  // !(OS_NACL | OS_MACOSX | OS_FREEBSD)
     pthread_setname_np(*state_->handle, thread_name.c_str());
-#endif  // !(OS_NACL | OS_MACOSX)
+#endif  // !(OS_NACL | OS_MACOSX | OS_FREEBSD)
   }
 }
 
