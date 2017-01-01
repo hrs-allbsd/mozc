24b79627a33fc8644d1469d4f0be7e3ffd09c268
Hiroki Sato <hrs@allbsd.org>
2017-01-01 14:00:18 +0900

    Make IsLinux() true on FreeBSD.

--- src/build_tools/util.py
+++ src/build_tools/util.py
@@ -56,7 +56,7 @@ def IsMac():
 
 def IsLinux():
   """Returns true if the platform is Linux."""
-  return os.name == 'posix' and os.uname()[0] == 'Linux'
+  return os.name == 'posix' and ( os.uname()[0] == 'Linux' or os.uname()[0] == 'FreeBSD' )
 
 
 def GetNumberOfProcessors():
