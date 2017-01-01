9dc301390afbd4f9db2e065c8705cdb8c995e189
Hiroki Sato <hrs@allbsd.org>
2017-01-01 13:38:10 +0900

    Fix build on GCC.

--- src/unix/ibus/mozc_engine.cc
+++ src/unix/ibus/mozc_engine.cc
@@ -221,7 +221,7 @@ bool GetSurroundingText(IBusEngine *engine,
   }
 
   const size_t selection_start = min(cursor_pos, anchor_pos);
-  const size_t selection_length = abs(info->relative_selected_length);
+  const size_t selection_length = ::abs(info->relative_selected_length);
   Util::SubStringPiece(surrounding_text, 0, selection_start)
       .CopyToString(&info->preceding_text);
   Util::SubStringPiece(surrounding_text, selection_start, selection_length)
