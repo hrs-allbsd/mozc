868e2a78e7b531ac565b60087900fc7f77a7288f
catlyons <catlyons@google.com>
2017-02-27 12:24:17 -0500

    Add std:: prefix to STL non-type names.
    BUG=#392
    REF_BUG=
    REF_CL=148656850
    REF_TIME=2017-02-27T12:24:17-05:00
    REF_TIME_RAW=1488216257 -0500

--- src/base/flags.cc
+++ src/base/flags.cc
@@ -347,7 +347,7 @@ uint32 ParseCommandLineFlags(int *argc, char*** argv, bool remove_flags) {
 #ifndef IGNORE_HELP_FLAG
       string help;
       PrintFlags(&help);
-      cout << help;
+      std::cout << help;
       exit(0);
 #endif
     }
@@ -368,7 +368,7 @@ uint32 ParseCommandLineFlags(int *argc, char*** argv, bool remove_flags) {
 #endif
     if (!SetFlag(key, value)) {
 #ifndef IGNORE_INVALID_FLAG
-      cerr << "Unknown/Invalid flag " << key << std::endl;
+      std::cerr << "Unknown/Invalid flag " << key << std::endl;
       exit(1);
 #endif
     }
